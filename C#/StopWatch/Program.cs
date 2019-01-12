6b3d260100b6dbe9c8efd178f0ed2501af56e71e
Bahman_Jafarov <jrbahmanjafarov@gmail.com>
2019-01-12 23:31:56 +0400

    .

---
+++ C#/StopWatch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LearnCSharp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new StopWatch();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.Write("'0' Exit, '1' Start, '2' Stop, '3' Show Time, '4' Reset, '5' Show List: ");
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "0": Console.WriteLine();              return;
+                    case "1": stopWatch.Start();                 break;
+                    case "2": stopWatch.Stop();                  break;
+                    case "3": stopWatch.ShowTime();              break;
+                    case "4": stopWatch.Reset();                 break;
+                    case "5": stopWatch.ShowList();              break;
+                    default: Console.WriteLine("Wrong choice!"); break;
+                }
+            }
+        }    
+    }    
+}
---
+++ C#/StopWatch/StopWatch.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearnCSharp
+{
+    public class StopWatch
+    {
+        private DateTime startTime;
+        private DateTime stopTime;
+        private TimeSpan timeSpan;
+        private bool started = false;
+        private List<TimeSpan> records = new List<TimeSpan>();
+
+        public void Start()
+        {
+            if (started)
+            {
+                Console.WriteLine("Time had started!");
+                return;
+            }
+                
+            startTime = DateTime.Now;
+            started = true;
+            Console.WriteLine("Time has started!");
+        }
+
+        public void Stop()
+        {
+            if(!started)
+            {
+                Console.WriteLine("Time has not started!");
+                return;
+            }
+
+            stopTime = DateTime.Now;
+            timeSpan += stopTime - startTime;
+            records.Add(timeSpan);
+            started = false;
+            Console.WriteLine("Time has stopped!");
+        }
+
+        public void ShowTime()
+        {
+            if(started)
+            {
+                Console.WriteLine("Time has not stopped!");
+                return;
+            }
+
+            Console.WriteLine(timeSpan);
+        }
+
+        public void Reset()
+        {
+            timeSpan = TimeSpan.Zero;
+            started = false;
+        }
+
+        public void ShowList()
+        {
+            int i = 0;
+            foreach (var time in records)
+            {
+                Console.WriteLine("{0}. {1}", ++i, time);
+            }
+        }
+    }
+}
